4c88f90ba30fe2df1dbcc3d9416eba00c3b8514c
Saheed Adeleye <aadesaed@gmail.com>
2024-08-22 10:20:50 +0100

    add TODO file, and basic testing facility

---
+++ test.py
@@ -0,0 +1,65 @@
+import unittest
+import pathlib
+
+from shutil import rmtree
+
+
+def setUp(argv: list[str]) -> None:
+    for f in argv:
+        p = pathlib.Path(f).absolute()
+        if not p.exists():
+            if p.is_dir():
+                p.mkdir(parents=True)
+            else:
+                if not p.parent.exists():
+                    p.parent.mkdir(parents=True)
+                p.touch()
+        else:
+            p.mkdir(parents=True) if p.is_dir() else p.touch()
+
+
+def tearDown(argv: list[str]) -> None:
+    # TODO: Implement using os.walk()
+    for f in argv:
+        p = pathlib.Path(f)
+        if p.exists():
+            if p.is_file():
+                p.unlink()
+                if pathlib.Path(p.parts[0]).is_dir():
+                    rmtree(p.parts[0])
+            else:
+                rmtree(p)
+
+
+class LiteSortError(Exception):
+    pass
+
+
+class TestNoop(unittest.TestCase):
+    def test_noop(self):
+        self.assertEqual(None, None)
+
+
+class TestSetupAndDelete(unittest.TestCase):
+    test_files = [
+        "test",
+        "file1.txt",
+        "file2.pdf",
+        "file3.zip",
+        "dir1/d/d/d/d/d/d/d//file4.xz",
+        "dir2/dir21/dir211/dir2111/dir21111/not_a_dir",
+    ]
+
+    def test__setup_and_delete(self):
+        setUp(self.test_files)
+        tearDown(self.test_files)
+
+        # make sure they don't exist
+        for f in self.test_files:
+            fp = pathlib.Path(f)
+            if fp.exists():
+                raise LiteSortError()
+
+
+if __name__ == "__main__":
+    unittest.main()
